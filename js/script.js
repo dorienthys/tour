1949115df55c499f20d44d8d84f2a4c5c2f92f0b
Ziggy Verstrepen <ziggy.verstrepen@gmail.com>
2015-01-09 22:41:52 +0100

    navigatie via keyboad en al enkele pagina's aangemaakt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,65 @@
  $(document).ready(function() {
- 	
+
+ 	//ID in variabele steken
+ 	var rightArrow = $('#right-arrow');
+ 	var topArrow = $('#top-arrow')
+ 	var leftArrow = $('#left-arrow');
+ 	var bottomArrow = $('#bottom-arrow')
+
+ 	//href uithalen en in andere variabele steken
+ 	rightUrl = rightArrow.attr("href");
+ 	topUrl = topArrow.attr("href");
+ 	leftUrl = leftArrow.attr("href");
+ 	bottomUrl = bottomArrow.attr("href");
+
+ 	console.log("Bottom= " + bottomUrl, "Left= " +  leftUrl, "Right= " +  rightUrl,"Top= " +  topUrl);
+
+ 	//als keyboard wordt gebruikt gaat hij checken welke
+ 	//key is ingedrukt en naargelang de key, gaat hij een
+ 	//ander functie uitvoeren 
+ 	$(document).on("keydown", function(evt){
+ 		var code = evt.keyCode;
+ 		console.log(code);
+
+ 		switch(code) {
+ 			case 37:
+ 				leftArrow();
+ 				break;
+ 			case 38:
+ 				topArrow();
+ 				break;
+ 			case 39:
+ 				rightArrow();
+ 				break;
+ 			case 40:
+ 				bottomArrow();
+ 				break;
+ 		}
+
+ 		//functie uitvoeren die bij keycode hoort
+ 		//als url undefined is --> niets doen
+ 		function leftArrow(){
+ 			if(leftUrl != undefined){
+ 				window.location = leftUrl;
+ 			}
+	 	}
+
+	 	function topArrow(){
+	 		if(topUrl != undefined){
+ 				window.location = topUrl;
+ 			}
+	 	}
+
+	 	function rightArrow(){
+	 		if(rightUrl != undefined){
+ 				window.location = rightUrl;
+ 			}
+	 	}
+
+	 	function bottomArrow(){
+	 		if(bottomUrl != undefined){
+ 				window.location = bottomUrl;
+ 			}
+	 	}
+ 	});
  });
